0518e0bc8c96782b6409618af0b98cfab12f494b
sanjay <sanjay@remind101.com>
2015-06-09 16:02:57 -0700

    Always tail logs

--- rabbitmq-cluster/server/startrabbit.sh
+++ rabbitmq-cluster/server/startrabbit.sh
@@ -2,12 +2,12 @@
 
 if [ -z "$CLUSTERED" ]; then
 	# if not clustered then start it normally as if it is a single server
-	/usr/sbin/rabbitmq-server	
+	/usr/sbin/rabbitmq-server -detached
 else
 	if [ -z "$CLUSTER_WITH" ]; then
 		# If clustered, but cluster with is not specified then again start normally, could be the first server in the
 		# cluster
-		/usr/sbin/rabbitmq-server
+		/usr/sbin/rabbitmq-server -detached
 	else
 		/usr/sbin/rabbitmq-server -detached
 		rabbitmqctl stop_app
@@ -17,9 +17,11 @@ else
 			rabbitmqctl join_cluster --ram rabbit@$CLUSTER_WITH
 		fi
 		rabbitmqctl start_app
-
-		# Tail to keep the a foreground process active..
-		tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
 	fi
 fi
+
+# Wait for logs
+sleep 5
+# Tail to keep the a foreground process active..
+tail -f /var/log/rabbitmq/rabbit\@$HOSTNAME.log
 
